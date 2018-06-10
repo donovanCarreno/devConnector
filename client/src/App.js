fd390b1f8346924a6aa840d9195e46295f4792e4
donovanCarreno <donovan_carreno@yahoo.com>
2018-06-10 14:02:02 -0500

    profile display

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,9 @@ import CreateProfile from './components/create-profile/CreateProfile'
 import EditProfile from './components/edit-profile/EditProfile'
 import AddExperience from './components/add-credentials/AddExperience'
 import AddEducation from './components/add-credentials/AddEducation'
+import Profiles from './components/profiles/Profiles'
+import Profile from './components/profile/Profile'
+import NotFound from './components/not-found/NotFound'
 
 import store from './store'
 
@@ -62,6 +65,8 @@ class App extends Component {
             <div className='container'>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:handle' component={Profile} />
               <Switch>
                 <PrivateRoute exact path='/dashboard' component={Dashboard} />
               </Switch>
@@ -76,6 +81,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path='/add-education' component={AddEducation} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path='/not-found' component={NotFound} />
               </Switch>
             </div>
             <Footer />
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types'
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types'
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -13,6 +13,21 @@ export const getCurrentProfile = () => dispatch => {
     .catch(e => dispatch({
       type: GET_PROFILE,
       payload: {}
+    }))
+}
+
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+  dispatch(setProfileLoading())
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res => dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    }))
+    .catch(e => dispatch({
+      type: GET_PROFILE,
+      payload: null
     }))
 }
 
@@ -74,6 +89,21 @@ export const deleteEducation = (id) => dispatch => {
     .catch(e => dispatch({
       type: GET_ERRORS,
       payload: e.response.data
+    }))
+}
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading())
+  axios
+    .get('/api/profile/all')
+    .then(res => dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    }))
+    .catch(e => dispatch({
+      type: GET_PROFILES,
+      payload: null
     }))
 }
 
